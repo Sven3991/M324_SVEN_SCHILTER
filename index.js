88c431f671e5994a2a9855203b65cf8c8f4f8ce7
Sven Schilter <sven.schilter@lehre.roche.com>
2026-03-10 14:13:44 +0100

    Formatting with Errors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-export const sum = (a, b) => {
+     export const sum = (a, b) => {
   var oldValue = 1
-  const unusedVariable = oldValue
+   const unusedVariable = oldValue
 
   if (a == b) {
-    return a
+return a
   }
 
   return a + b + 1
-}
+            }
 
 console.log(sum(1, 2))
-console.log(myUndefinedVariable)
+  console.log(myUndefinedVariable)
